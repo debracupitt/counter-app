fb2e16b62d2cf70d056efd3cc663a36cf4089d32
Debra Cupitt <debracupitt@gmail.com>
2018-10-09 16:33:06 +0700

    bind this constructor in event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,29 +3,49 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    // tags: ["tag1", "tag2", "tag3"]
-    tags: []
+    imageUrl: "https://picsum.photos/75",
+    tags: ["tag1", "tag2", "tag3"]
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags.</p>;
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  handleIncrement = () => {
+    this.state.count++;
+    console.log("Increment Clicked", this.state.count);
+  };
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag."}
-        {this.renderTags()}
+        <img className="m-2" src={this.state.imageUrl} alt="" />
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          +
+        </button>
+        {/* <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul> */}
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
