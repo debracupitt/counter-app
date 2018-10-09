828f482af4bb9ef4fe41ff346d81f8373e5a1ff2
Debra Cupitt <debracupitt@gmail.com>
2018-10-09 16:20:34 +0700

    add text with && operator

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,35 +2,30 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
-    imageUrl: "https://picsum.photos/75",
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0,
+    // tags: ["tag1", "tag2", "tag3"]
+    tags: []
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags.</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div>
-        <img className="m-2" src={this.state.imageUrl} alt="" />
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">+</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag."}
+        {this.renderTags()}
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
