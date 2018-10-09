11ea45493d316ee92e2dfdd745d1d473bf07a6b7
Debra Cupitt <debracupitt@gmail.com>
2018-10-09 14:50:50 +0700

    remove styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,30 +2,24 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
     imageUrl: "https://picsum.photos/75"
-  };
-
-  styles = {
-    fontSize: 18,
-    fontWeight: "bold"
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <img className="m-2" src={this.state.imageUrl} alt="" />
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
-        <button
-          style={{ fontSixe: 30, fontWeight: "bold" }}
-          className="btn btn-secondary btn-sm"
-        >
-          +
-        </button>
-      </React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">+</button>
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
