ad9aad452137309f7a0dc4e54859347423cbc862
Debra Cupitt <debracupitt@gmail.com>
2018-10-11 16:39:27 +0700

    add children properties to react element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     imageUrl: "https://picsum.photos/75",
     tags: ["tag1", "tag2", "tag3"]
   };
@@ -13,12 +13,15 @@ class Counter extends Component {
   // }
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log(this.props);
+
     return (
       <div>
+        {this.props.children}
         <img className="m-2" src={this.state.imageUrl} alt="" />
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
@@ -38,13 +41,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
