50f9f956a615156e795340aa09be7ea9f2c58b54
Debra Cupitt <debracupitt@gmail.com>
2018-10-11 21:05:20 +0700

    refactor with object destructuring in navbar and counters components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,32 +1,17 @@
 import React, { Component } from "react";
 
 // Stateless Functional Component - define a function that returns a react element
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Number of items in your cart:
         <span className="badge badge-pill badge-secondary m-1">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
   );
 };
-
-// class NavBar extends Component {
-//   render() {
-//     return (
-//       <nav className="navbar navbar-light bg-light">
-//         <a className="navbar-brand" href="#">
-//           Number of items in your cart:
-//           <span className="badge badge-pill badge-secondary m-1">
-//             {this.props.totalCounters}
-//           </span>
-//         </a>
-//       </nav>
-//     );
-//   }
-// }
 
 export default NavBar;
